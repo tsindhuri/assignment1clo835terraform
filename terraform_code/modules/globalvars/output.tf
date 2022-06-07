30c75094fe3996352157ab0d9bd41473e9e14f6e
EC2 Default User <ec2-user@ip-172-31-71-62.ec2.internal>
2022-06-07 16:56:11 +0000

    terraform code

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Tejaswi"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "week1"
+}
